ce7fc53c1b1ffbe021104598deb2e82f3acb1219
Peter <petermounir97@gmail.com>
2020-11-25 23:48:00 +0200

    Added Animation

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flash_chat/constants.dart';
 
 class ChatScreen extends StatefulWidget {
+
+  static String id = 'Chat Screen';
+
   @override
   _ChatScreenState createState() => _ChatScreenState();
 }
